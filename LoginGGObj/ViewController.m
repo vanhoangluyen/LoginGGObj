e13bf7c9aa0e03def44e7a0da55ac1932e6b109f
luyenhoangvan <luyenhv@mobgame.vn.com>
2018-02-09 12:19:29 +0700

    Initial Commit

---
+++ LoginGGObj/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LoginGGObj
+//
+//  Created by LuyenBG on 2/9/18.
+//  Copyright © 2018 LuyenBG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
